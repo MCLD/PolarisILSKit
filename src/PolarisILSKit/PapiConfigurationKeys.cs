8c8e23be95c39040c37a753c8993f97a50f36bac
Dan Wilcox <danielwilcox@mcldaz.org>
2019-11-04 12:14:28 -0700

    Add Papi Helper

---
+++ src/PolarisILSKit/PapiConfigurationKeys.cs
@@ -0,0 +1,16 @@
+﻿namespace PolarisILSKit
+{
+    public struct PapiConfigurationKeys
+    {
+        public static readonly string SectionName = "PAPI";
+
+        public static readonly string AccessID = "AccessID";
+        public static readonly string AccessKey = "AccessKey";
+        public static readonly string HostName = "HostName";
+        public static readonly string Domain = "Domain";
+        public static readonly string UserName = "UserName";
+        public static readonly string Password = "Password";
+        public static readonly string UserId = "UserId";
+        public static readonly string WorkstationId = "WorkstationId";
+    }
+}
---
+++ src/PolarisILSKit/PapiHelper.cs
@@ -0,0 +1,38 @@
+﻿using Clc.Polaris.Api;
+using Clc.Polaris.Api.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace PolarisILSKit
+{
+    public class PapiHelper : PapiClient
+    {
+        public int UserId { get; set; }
+        public int WorkstationId { get; set; }
+
+        public PapiHelper(IConfigurationSection config)
+        {
+            AccessID = config[PapiConfigurationKeys.AccessID];
+            AccessKey = config[PapiConfigurationKeys.AccessKey];
+            Hostname = config[PapiConfigurationKeys.HostName];
+            StaffOverrideAccount = new PolarisUser
+            {
+                Domain = config[PapiConfigurationKeys.Domain],
+                Username = config[PapiConfigurationKeys.UserName],
+                Password = config[PapiConfigurationKeys.Password]
+            };
+
+            UserId = config.GetValue<int>(PapiConfigurationKeys.UserId);
+            WorkstationId = config.GetValue<int>(PapiConfigurationKeys.WorkstationId);
+        }
+
+        public bool IsConfigured()
+        {
+            return !string.IsNullOrEmpty(AccessID)
+                && !string.IsNullOrEmpty(AccessKey)
+                && !string.IsNullOrEmpty(Hostname)
+                && StaffOverrideAccount != null
+                && UserId > 0
+                && WorkstationId > 0;
+        }
+    }
+}
